291d39f3f6461267ae932bfefe061bd6217504d5
hernandezpablo <67705466+hernandezpablo@users.noreply.github.com>
2022-03-21 22:16:53 -0300

    Ejercicio en clase
    Ejerecicio 1 comienzo

---
+++ Ejercicio 1/Ejercicio 1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Ejercicio_1
+{
+    internal class Program
+    {
+        /*Ingresar 5 números por consola, guardándolos en una variable escalar.
+        Luego calcular y mostrar: el valor máximo, el valor mínimo y el promedio.*/
+        static void Main(string[] args)
+        {
+            int numero;
+            string numeroTexto;
+            bool resultado;
+
+            do
+            {
+                Console.WriteLine("Ingrese un numero:");
+                numeroTexto = Console.ReadLine();
+                resultado = int.TryParse(numeroTexto, out numero);
+            } while (!resultado);
+
+            Console.WriteLine("El numero ingresado fue {0}", numero);
+        }
+    }
+}
